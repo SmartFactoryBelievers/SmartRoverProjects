a508a7c744169dc490ec77f2d597c04ec94a561f
Smart Factory Believers <128001053+SmartFactoryBelievers@users.noreply.github.com>
2023-06-30 13:11:46 -0500

    Update Project01_BlinkingLED.py
    Code cleanup

--- Project01_BlinkingLED.py
+++ Project01_BlinkingLED.py
@@ -10,8 +10,8 @@
 
 #Importing libraries
 # Libraries are defined sets of code for specific uses
-# Here we want the sleep function for timing and GPIO for the Pi's pin from time import sleep
-import RPi.GPIO as GPIO
+from time import sleep #here we import the sleep function to act as a timer
+import RPi.GPIO as GPIO #here we import the GPIO library to activate the Raspberry Pi computer
 
 # Let's define variables so we can use them later
 # Variables are words that take on values within the code
@@ -21,10 +21,11 @@ LED_Pin = 7 #the internal Pi pin number that goes to snap 7
 #------------------------ CHALLENGE 1: CHANGE THE VALUES OF LED_ON AND LED_OFF ----------------------
 LED_On = .2 #duration of LED flash, seconds
 LED_Off = .1 #duration in between flashes, seconds
+#-------------------------------------- END OF CHALLENGE 1 ------------------------------------------
 
 #Setting up our pin
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(LED_Pin, GPIO.OUT, initial=GPIO.LOW) #Output pin, start off
+GPIO.setup(LED_Pin, GPIO.OUT, initial=GPIO.LOW) #This line says we want to use the LED_Pin and start with the LED off
 while True: #Looping over and over again
 sleep(LED_Off) #Keep LED off for defined duration
 GPIO.output(LED_Pin, GPIO.HIGH) #Turn lED on
