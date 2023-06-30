8e69837d2e4fb97bf16bdf2144dbcb865fbcbcb0
Smart Factory Believers <128001053+SmartFactoryBelievers@users.noreply.github.com>
2023-06-30 14:44:32 -0500

    Update Project06_LightActivatedDriving.py
    code cleanup

--- Project06_LightActivatedDriving.py
+++ Project06_LightActivatedDriving.py
@@ -1,7 +1,7 @@
 # Project 6
 # Learning to program, writing functions, using motor control outputs, adding loop complexity
 # Build the the Project 6 circuit and have the rover be controlled by ambient light
-# Turn down the ligth and point a flashlight at the rover to direct it
+# Turn down the light and point a flashlight at the rover to direct it
 
 #Challenge 1
 # Try changing the drive functions to switch the driving directions
@@ -49,42 +49,45 @@ GPIO.setup(Photo_Pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 #Let's write some driving functions we can use later
 def drive_forward(time):
-GPIO.output(Left_Forward_Pin, GPIO.HIGH) #Left motor forward
-GPIO.output(Right_Forward_Pin, GPIO.HIGH) #Right motor forward
-sleep(time)
-GPIO.output(Left_Forward_Pin, GPIO.LOW) #Left motor off
-GPIO.output(Right_Forward_Pin, GPIO.LOW) #Right motor off
-print('forward')
-sleep(1)
+  GPIO.output(Left_Forward_Pin, GPIO.HIGH) #Left motor forward
+  GPIO.output(Right_Forward_Pin, GPIO.HIGH) #Right motor forward
+  sleep(time)
+  GPIO.output(Left_Forward_Pin, GPIO.LOW) #Left motor off
+  GPIO.output(Right_Forward_Pin, GPIO.LOW) #Right motor off
+  print('forward')
+  sleep(1)
+  
 def drive_left_turn(time):
-GPIO.output(Left_Backward_Pin, GPIO.HIGH) #Left motor backward
-GPIO.output(Right_Forward_Pin, GPIO.HIGH) #Right motor forward
-sleep(time)
-GPIO.output(Left_Backward_Pin, GPIO.LOW) #Left motor off
-GPIO.output(Right_Forward_Pin, GPIO.LOW) #Right motor off
-print('left turn')
-sleep(1)
+  GPIO.output(Left_Backward_Pin, GPIO.HIGH) #Left motor backward
+  GPIO.output(Right_Forward_Pin, GPIO.HIGH) #Right motor forward
+  sleep(time)
+  GPIO.output(Left_Backward_Pin, GPIO.LOW) #Left motor off
+  GPIO.output(Right_Forward_Pin, GPIO.LOW) #Right motor off
+  print('left turn')
+  sleep(1)
+  
 def drive_right_turn(time):
-GPIO.output(Left_Forward_Pin, GPIO.HIGH) #Left motor forward
-GPIO.output(Right_Backward_Pin, GPIO.HIGH) #Right motor backward
-sleep(time)
-GPIO.output(Left_Forward_Pin, GPIO.LOW) #Left motor off
-GPIO.output(Right_Backward_Pin, GPIO.LOW) #Right motor off
-print('right turn')
-sleep(1)
+  GPIO.output(Left_Forward_Pin, GPIO.HIGH) #Left motor forward
+  GPIO.output(Right_Backward_Pin, GPIO.HIGH) #Right motor backward
+  sleep(time)
+  GPIO.output(Left_Forward_Pin, GPIO.LOW) #Left motor off
+  GPIO.output(Right_Backward_Pin, GPIO.LOW) #Right motor off
+  print('right turn')
+  sleep(1)
+  
 def drive_backward(time):
-GPIO.output(Left_Backward_Pin, GPIO.HIGH) #Left motor backward
-GPIO.output(Right_Backward_Pin, GPIO.HIGH) #Right motor backward
-sleep(time)
-GPIO.output(Left_Backward_Pin, GPIO.LOW) #Left motor off
-GPIO.output(Right_Backward_Pin, GPIO.LOW) #Right motor off
-print('backward')
-sleep(1)
+  GPIO.output(Left_Backward_Pin, GPIO.HIGH) #Left motor backward
+  GPIO.output(Right_Backward_Pin, GPIO.HIGH) #Right motor backward
+  sleep(time)
+  GPIO.output(Left_Backward_Pin, GPIO.LOW) #Left motor off
+  GPIO.output(Right_Backward_Pin, GPIO.LOW) #Right motor off
+  print('backward')
+  sleep(1)
 
 # For challenge 4, we will use a dummy variable to help with modulo operator
 count = 0
-# Replace the True with the modulo operator statement as %, which means remainder in division
-# So modulo 2 keeps track of odd and even presses since even divided by 2 has remainder of 0
+#The modulo operator statement as %, which means remainder in division
+# So modulo of 2 keeps track of odd and even presses since even divided by 2 has remainder of 0
 # To use this as a logical, let's try count % 2 == 0
 
 # For challenge 5, we will set a maximum light search time for the loop
@@ -94,29 +97,31 @@ Max_Search_Time = 4 #seconds
 
 
 while True: # Continuous outer while loop
-sleep(0.25)
-count = count + 1 # Increment the counter for the modulo
-# If the phototransistor detects enough light, drive towards it
-if GPIO.input(Photo_Pin):
-
-#------------------------ CHALLENGE 1: CHANGE THE VALUES BELOW TO DRIVE IN NEW PATTERNS ----------------------
-#------------------------ CHALLENGE 2: GET CREATIVE AND ADD NEW DRIVING FUNCTIONS TO CHANGE THE LIGHT-SEEKING SPIN PATTERNS ----------------------
-drive_forward(Forward_Time)
-# If there's not enough light, let's look for it by spinning the rover
-else:
-# For challenge 5, we can use the timer function to control the light seach
-Start_Time = time.time()
-while not(GPIO.input(Photo_Pin)):
-Elapsed_Time = round(time.time() - Start_Time,2)
-print('Not enough light, searching for more')
-
-#------------------------ CHALLENGE 4: REPLACE THE "True" WITH THE MODULO OPERATOR ----------------------
-#------------------------ CHALLENGE 5: REPLACE THE "True" WITH A COMPARATIVE (<) BETWEEN Elapsed_Time and Max_Search_Time ----------------------
-if True:
-drive_left_turn(Left_Turn_Time)
-sleep(Wait_Time)
-else: # For challenge 4, modulo uses these drive commands on odd loops
-drive_right_turn(Right_Turn_Time)
-sleep(Wait_Time)
-else:
+  sleep(0.25)
+  count = count + 1 # Increment the counter for the modulo
+  
+  # If the phototransistor detects enough light, drive towards it
+  if GPIO.input(Photo_Pin):
+    #------------------------ CHALLENGE 1: CHANGE THE VALUES BELOW TO DRIVE IN NEW PATTERNS ----------------------
+    #------------------------ CHALLENGE 2: GET CREATIVE AND ADD NEW DRIVING FUNCTIONS TO CHANGE THE LIGHT-SEEKING SPIN PATTERNS ----------------------
+    drive_forward(Forward_Time)
+    
+    # If there's not enough light, let's look for it by spinning the rover
+  else:
+    # For challenge 5, we can use the timer function to control the light seach
+    Start_Time = time.time()
+    while not(GPIO.input(Photo_Pin)):
+    Elapsed_Time = round(time.time() - Start_Time,2)
+    print('Not enough light, searching for more')
+  
+      #------------------------ CHALLENGE 4: REPLACE THE "True" WITH THE MODULO OPERATOR ----------------------
+      #------------------------ CHALLENGE 5: REPLACE THE "True" WITH A COMPARATIVE (<) BETWEEN Elapsed_Time and Max_Search_Time ----------------------
+      if True:
+        drive_left_turn(Left_Turn_Time)
+        sleep(Wait_Time)
+        
+      else: # For challenge 4, modulo uses these drive commands on odd loops
+        drive_right_turn(Right_Turn_Time)
+        sleep(Wait_Time)
+    else:
 break # Exits the loop after Max Search Time exceeded
