7b1c4105ed0481d90959056c610b1ddc2fbb558f
Smart Factory Believers <128001053+SmartFactoryBelievers@users.noreply.github.com>
2024-06-10 16:30:20 -0500

    Update Project01_BlinkingLED.py

--- Project01_BlinkingLED.py
+++ Project01_BlinkingLED.py
@@ -25,38 +25,65 @@ from time import sleep
 import RPi.GPIO as GPIO 
 #   - Here we import the GPIO library to activate the Raspberry Pi computer.
 
-# Step 2: Variables (READ-ONLY)
+# Step 2: Variables 
+
 #   - Variables are words that take on values for our code.
 #   - You can think of them as boxes that we store numbers in, and we'll later take the number out of the box when we want to use it!
 
 #   - Here, we'll define some variables for our LED that we'll use later.
-#   - Each Raspberry Pi has 40 physical pins within it. The number 7 corresponds to one of those pins within it. Snap 7 is connected to Pin 7 by shear coincidence.
+#   - If we change the definition of a variable, it will change the value of the variable throughout the code.
+
 LED_Pin = 7 # The internal Pi pin number that goes to snap 7
+#   - Each Raspberry Pi has 40 physical pins within it. The number 7 corresponds to one of those pins within it. Snap 7 is connected to Pin 7 by shear coincidence.
+
+#   - This is a challenge. The code surrounded by the challenge is what will need to be modified to complete the challenge.
+#   - The LED_On and LED_Off variables are what control how long the LED flashes for. To complete the challenge, try changing the values and see what happens.
 
 #------------------------ CHALLENGE 1: CHANGE THE VALUES OF LED_ON AND LED_OFF ----------------------
 LED_On = .2 # Duration of LED flash, seconds
 LED_Off = .1 # Duration in between flashes, seconds
 #-------------------------------------- END OF CHALLENGE 1 ------------------------------------------
 
-# Step 3: Raspberry Pi Setup (READ-ONLY)
+# Step 3: Raspberry Pi Setup 
+
 #   - We'll setup our Pi to use the right pins and other details.
-#   - GPIO.setmode deals with assigning a certain pin configuration to the Pi. We are using GPIO.BOARD as our configuration.
+#   - Raspberry Pis can be set up in multiple different ways called configurations
+#   - GPIO.setmode assigns a certain pin configuration to the Pi. We are using GPIO.BOARD as our configuration.
+ 
 GPIO.setmode(GPIO.BOARD)
+
 #   - Each pin can either be an input (ex. receive a signal from a button) or an output (ex. send a signal to a motor)
 #   - A default state also needs to be declared (does it have to be turned on initially or turned off initially?)
-GPIO.setup(LED_Pin, GPIO.OUT, initial=GPIO.LOW) # This line says we want to use the LED_Pin (in this case, 7) as an output and start with the LED off.
+
+GPIO.setup(LED_Pin, GPIO.OUT, initial=GPIO.LOW) 
+#   - This line says we want to use the LED_Pin (in this case, 7) as an output and start with the LED off.
 
 # Step 4: Run the Code (ACTIVITY)
-#   - This is where we'll do the main steps of our program.
-while True: # This line says we'll loop over and over again, until we end the program.
-    sleep(LED_Off) # Pause the program for the defined duration, keeping the LED off 
+#   - This is where we write the program that actually turns the LED on and off
+#   - You can think of the all the code before this as preparation for this portion
 
-    GPIO.output(LED_Pin, GPIO.HIGH) #Turn LED on
-    sleep(LED_On) #Pause the program for the defined duration, keeping the LED on 
+while True: 
+#   - While is a loop, which causes the code within the loop to repeat over and over again
+#   - The code indented beneath the While statement is considered within the loop
+#   - A While loop will continue to loop until a condition is met, which is placed after the "While" but before the colon
+#   - A True condition means the code will loop endlessly, because it is always True
+
+    sleep(LED_Off)
+    #   - Pause the program for the defined duration, keeping the LED off
+
+    GPIO.output(LED_Pin, GPIO.HIGH) 
+    #   - This line takes the state listed (GPIO.HIGH) and applies it to the chosen pin (LED_Pin)
+    #   - This is what turns on the LED in the circuit, since the pin that supplies power to the LED is LED_Pin
+    
+    sleep(LED_On) 
+    #   - Pause the program for the defined duration, keeping the LED on 
 
     GPIO.output(LED_Pin, GPIO.LOW) #Turn LED off
+    #   - This line takes the state listed (GPIO.LOW) and applies it to the chosen pin (LED_Pin)
+    #   - This is what turns off the LED in the circuit, since the pin that supplies power to the LED is LED_Pin
 
     # NOTE: We'll repeat the four lines above, in the while True, over and over again. This is an easy way to code our program to keep running!
+
 # Hit RUN on the Program and Look for a Blinking Light!
 
 ##############
