42bdecbf4968627a9176555b3436267faa976306
CarsonLittle1026 <109241524+CarsonLittle1026@users.noreply.github.com>
2025-02-20 11:37:25 -0800

    Update Project04_ProgrammedDriving.py
    Updated with latest code form DEV (Issue #7)

--- Project04_ProgrammedDriving.py
+++ Project04_ProgrammedDriving.py
@@ -1,175 +1,98 @@
-######################################################################################
-# Project 4                                                                          #
-# Learning Objective: Learn to program functions that control the rover motors as    #
-#     well as learn how to use for loops in programming.                             #
-# Introduction: For this project, the goal will be to program the rover to drive for #
-#     a set amount of time on a programmable path. The main concepts covered in this #
-#     project are "for" loops and motor controls.                                    #
-# For this project, you WILL need to modify code to get it to run properly. The      #
-#      sections that need to be changed will be called out in the code               #
-#      You WILL need to modify code to complete the challenges listed at the end of  #
-#      the project                                                                   #
-######################################################################################
+# Project 4
 
-# Step 1: Importing Libraries
+# Learning to program, writing functions, and using motor control outputs
 
-#   - Here we want the sleep function for timing and GPIO for the Pi's pins.
-#   - These are the same functions and libraries that we used in the previous project
+# Build the the Project 4 circuit and drive the rover along your designed path
 
+#Challenge 1
+# Try changing the drive time variable to create a new driving path
+
+#Challenge 2
+# Reorder the drive functions to create a new driving path
+
+#Challege 3
+# Create your own custom driving path using the different drive functions and time arguments
+
+#Importing libraries
+# Here we want the sleep function for timing and GPIO for the Pi's pins
 from time import sleep
 import RPi.GPIO as GPIO
 
-# Step 2: Variables
+GPIO.setwarnings(False)
 
-Left_Forward_Pin = 35 
-#   - The internal Pi pin number that goes to snap 1
-#   - This variable will be used to send signals to the rover motors
+#Let's define variables so we can use them later
+Left_Forward_Pin =  35 #the internal Pi pin number that goes to snap 1
+Left_Backward_Pin =  31 #the internal Pi pin number that goes to snap 2
+Right_Forward_Pin =  26 #the internal Pi pin number that goes to snap 3
+Right_Backward_Pin =  21 #the internal Pi pin number that goes to snap 4
 
-Left_Backward_Pin = 31 
-#   - The internal Pi pin number that goes to snap 2
-#   - This variable will be used to send signals to the rover motors
-
-Right_Forward_Pin = 26 
-#   - The internal Pi pin number that goes to snap 3
-#   - This variable will be used to send signals to the rover motors
-
-Right_Backward_Pin = 21 
-#   - The internal Pi pin number that goes to snap 4
-#   - This variable will be used to send signals to the rover motors
-
-
-#   - Below we can define the timing variables for the driving functions, in seconds
-#   - These variable all store values for timing and are not related to pins
-
-#------------------------ CHALLENGE 1: CHANGE THE VALUES BELOW TO DRIVE IN NEW PATTERNS ----------------------
+#Here we can define the timing variables for the driving functions, in seconds
+# For challenge 1, we can try different values here to drive in new patterns
 Forward_Time = 2
 Backward_Time = 1
 Left_Turn_Time = 0.5
 Right_Turn_Time = 0.5
 Wait_Time = 1
-#-------------------------------------- END OF CHALLENGE 1 ------------------------------------------
 
-# Step 3: Raspberry Pi Setup
-
-#   - Here is where we set up and configure our Raspberry Pi
-#   - First, we are going to assign the BOARD pin configuration
-
+#Setting up our pins
 GPIO.setmode(GPIO.BOARD)
-
-#   - Next, we are going to set up our pins like in previous projects
-
+#Our output pins, start off
 GPIO.setup(Left_Forward_Pin, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(Left_Backward_Pin, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(Right_Forward_Pin, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(Right_Backward_Pin, GPIO.OUT, initial=GPIO.LOW)
 
-# Step 4: Functions
 
-#   - Let's write 4 driving functions we can use later to program a driving path.
-#   - To review functions, look at Project 3 for more info
+#Let's write some driving functions we can use later to program a driving path
+def drive_forward(time):    
+    GPIO.output(Left_Forward_Pin, GPIO.HIGH) #Left motor forward
+    GPIO.output(Right_Forward_Pin, GPIO.HIGH) #Right motor forward
+    sleep(time)
+    GPIO.output(Left_Forward_Pin, GPIO.LOW) #Left motor off
+    GPIO.output(Right_Forward_Pin, GPIO.LOW) #Right motor off
+    print('forward')
+    sleep(1)
 
-def drive_forward(time):
-  #   - Function turns on two of the four motors for the duration of the sleep value.
-  #   - Then, turns it off and prints a "forward" message in the command line
-  
-  GPIO.output(Left_Forward_Pin, GPIO.HIGH) 
-  #   - Move left motor forward
-  GPIO.output(Right_Forward_Pin, GPIO.HIGH) 
-  #   - Move right motor forward
-  sleep(time) 
-  #   - Wait for "time" seconds. This controls how long we move the motors for!
-
-  GPIO.output(Left_Forward_Pin, GPIO.LOW) 
-  #   - Turn left motor off
-  GPIO.output(Right_Forward_Pin, GPIO.LOW) 
-  #   - Turn right motor off
-  print('forward')
-  sleep(1) 
-  #   - Pause before our next function
-  
 def drive_left_turn(time):
-  #   - Function turns on two of the four motors for the duration of the sleep value.
-  #   - Then, turns it off and prints a "left turn" message in the command line
-  
-  GPIO.output(Left_Backward_Pin, GPIO.HIGH) 
-  #   - Move left motor backward
-  GPIO.output(Right_Forward_Pin, GPIO.HIGH) 
-  #   - Move right motor forward
-  sleep(time) 
-  #   - Wait for "time" seconds. This controls how long we move the motors for!
-
-  GPIO.output(Left_Backward_Pin, GPIO.LOW) 
-  #   - Turn left motor off
-  GPIO.output(Right_Forward_Pin, GPIO.LOW) 
-  #   - Turn right motor off
-  print('left turn')
-  sleep(1) 
-  #   - Pause before our next function
-  
+    GPIO.output(Left_Backward_Pin, GPIO.HIGH) #Left motor backward
+    GPIO.output(Right_Forward_Pin, GPIO.HIGH) #Right motor forward
+    sleep(time)
+    GPIO.output(Left_Backward_Pin, GPIO.LOW) #Left motor off
+    GPIO.output(Right_Forward_Pin, GPIO.LOW) #Right motor off
+    print('left turn')
+    sleep(1)
+    
 def drive_right_turn(time):
-  #   - Function turns on two of the four motors for the duration of the sleep value.
-  #   - Then, turns it off and prints a "right turn" message in the command line 
-  
-  GPIO.output(Left_Forward_Pin, GPIO.HIGH) 
-  #   - Move left motor forward
-  GPIO.output(Right_Backward_Pin, GPIO.HIGH) 
-  #   - Move right motor backward
-  sleep(time) 
-  #   - Wait for "time" seconds. This controls how long we move the motors for!
-
-  GPIO.output(Left_Forward_Pin, GPIO.LOW) 
-  #   - Turn left motor off
-  GPIO.output(Right_Backward_Pin, GPIO.LOW) 
-  #   - Turn right motor off
-  print('right turn')
-  sleep(1) 
-  #   - Pause before our next function
-
-#   - Can you finish the function by filling in the blanks for the pins and states? 
-#   - Replace the question marks with variables and then uncomment lines 132 - 139
-#   - This is a backward driving function, so both backward pins should be set High then Low.
-
+    GPIO.output(Left_Forward_Pin, GPIO.HIGH) #Left motor forward
+    GPIO.output(Right_Backward_Pin, GPIO.HIGH) #Right motor backward
+    sleep(time)
+    GPIO.output(Left_Forward_Pin, GPIO.LOW) #Left motor off
+    GPIO.output(Right_Backward_Pin, GPIO.LOW) #Right motor off
+    print('right turn')
+    sleep(1)
+    
+# Can you finish the function by filling in the blanks for the pins and states?
+# This is a backward driving function, so both backward pins should be High then Low
+# Uncomment the code when complete
 def drive_backward(time):
-  
-  #------------ NOW YOU TRY: REPLACE THE ?? WITH THE CORRECT VARIABLES AND UNCOMMENT -------------
-  # GPIO.output(??, GPIO.???) #Move left motor backward
-  # GPIO.output(??, GPIO.???) #Move right motor backward
-  # sleep(time) # Wait for "time" seconds. This controls how long we move the motors for!
+    #GPIO.output(??, GPIO.???) #Left motor backward
+    #GPIO.output(??, GPIO.???) #Right motor backward
+    #sleep(time)
+    #GPIO.output(??, GPIO.???) #Left motor off
+    #GPIO.output(??, GPIO.???) #Right motor off
+    #print('backward')
+    #sleep(1)
 
-  # GPIO.output(??, GPIO.???) # Turn left motor off
-  # GPIO.output(??, GPIO.???) # Turn right motor off
-  # print('backward')
-  # sleep(1) # Pause before our next function
-  #-------------------------------------- END OF EXERCISE ----------------------------------------
-
-#Step 5: Run the Program
-  
-#   - Here we use a "for" loop to control the number of times the code is executed.
-#   - If we increase the range, the code will loop multiple times.
-
-#------- NOW YOU TRY: CHANGE THE VALUE IN range() TO INCREASE THE NUMBER OF LOOPS PERFORMED ------
+#Here we can use a for loop to control the number of times the code is executed
+# Changing the value of range() increases the number of loops performed
 for n in range(1):
-  #   - Let's use the driving functions defined above to create a driving path
-  #   - Feel free to change the order and timing variables. See what paths you can make!
-  sleep(Wait_Time)
-  drive_forward(Forward_Time)
-  drive_left_turn(Left_Turn_Time)
-  drive_backward(Backward_Time)
-  drive_right_turn(Right_Turn_Time)
+    
+    # Let's use the driving functions defined above to create a driving path
+    # For challenges 2 and 3, try changing the driving functions and order here
+    sleep(Wait_Time)
+    drive_forward(Forward_Time)
+    drive_left_turn(Left_Turn_Time)
+    drive_backward(Backward_Time)
+    drive_right_turn(Right_Turn_Time)
 
-  #---------- CHALLENGE 2: REORDER THE DRIVE FUNCTIONS TO MAKE A NEW DRIVING PATH  ---------------
-  #-- CHALLENGE 3: USE DIFFERENT DRIVE FUNCTIONS AND TIME ARGUMENTS TO MAKE A NEW DRIVING PATH  --
-
-#   - Now that you  have read through the project, hit Run and see what path the rover takes!
-#   - Remember, code reads downward, so the functions at the top will be performed first!
-
-##############
-# Challenges #
-##############
-
-# Challenge 1: Try changing the driving time variables to create a new driving path.
-#   - Q: Can you control what angle the rover turns at by increasing or decreasing the time?
-
-# Challenge 2: Reorder the drive functions to create a new driving path.
-
-# Challenge 3: Create your own custom driving path using different drive functions and time arguments.
+GPIO.cleanup()
